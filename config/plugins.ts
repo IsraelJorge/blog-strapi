87366ba4386fd77507b8e140d0ce232089fbd81b
IsraelJorge <israeljorge2000@gmail.com>
2023-07-07 10:57:59 -0300

    feat: add plugin deploy vercel

--- config/plugins.ts
+++ config/plugins.ts
@@ -17,7 +17,7 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      // deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
       apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
       appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
       teamFilter: env("VERCEL_DEPLOY_PLUGIN_TEAM_FILTER"),
